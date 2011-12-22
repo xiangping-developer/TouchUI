97953f225e046568936edaa35a0effde917b68e6
Xiangping Meng <mengxiangpingx@gmail.com>
2011-12-22 17:25:27 +0800

    init

---
+++ InfinityScrollView/InfinityScrollView.h
@@ -0,0 +1,13 @@
+//
+//  InfinityScrollView.h
+//  InfinityScrollView
+//
+//  Created by Meng Xiangping on 12/22/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfinityScrollView : UIView
+
+@end
---
+++ InfinityScrollView/InfinityScrollView.m
@@ -0,0 +1,31 @@
+//
+//  InfinityScrollView.m
+//  InfinityScrollView
+//
+//  Created by Meng Xiangping on 12/22/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "InfinityScrollView.h"
+
+@implementation InfinityScrollView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
